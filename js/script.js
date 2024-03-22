e59d78a445536c570170d21743044385359ed468
Carlos Mozart <carlosmozartbna@gmail.com>
2024-03-22 12:00:14 -0300

    Versão 3.0.0
    Otimização do código e atualização para os scripts mais atuais utilizados para a o provisonamento das ONUs nas OLTs Huawei.

--- js/script.js
+++ js/script.js
@@ -36,7 +36,7 @@ class GetValores {
         document.getElementById('servicePort2').value = '';
         document.getElementById('vlan').value = '';
 
-        if (base == 'WLAN' && olt == 'HUAWEI' && olt2 == 'DISTRITO' && lineProfile == 'ROUTER') {
+        if (base == 'WLAN' && olt == 'HUAWEI' && olt2 == 'WLAN' && lineProfile == 'ROUTER') {
             const resultadoLinhaB2 = document.createElement('li');
             resultadoLinhaB2.textContent = `conf`;
             this.resultados.appendChild(resultadoLinhaB2);
@@ -50,78 +50,11 @@ class GetValores {
             this.resultados.appendChild(resultadoRamo);
 
             const resultadonOnu = document.createElement('li');
-            resultadonOnu.textContent = `ont port native-vlan ${ramo} ${nOnu} eth 1 vlan ${vlan}`;
+            resultadonOnu.textContent = `ont ipconfig ${ramo} ${nOnu} ip-index 1 dhcp vlan 110 priority 5`;
             this.resultados.appendChild(resultadonOnu);
 
             const resultadoLinhaB6 = document.createElement('li');
-            resultadoLinhaB6.textContent = `ont port native-vlan ${ramo} ${nOnu} eth 1 vlan 110`;
-            this.resultados.appendChild(resultadoLinhaB6);
-
-            const resultadoLinhaB3 = document.createElement('li');
-            resultadoLinhaB3.textContent = `service-port vlan ${vlan} gpon ${placa}/${ramo} ont ${nOnu} gemport 126 multi-service user-vlan ${vlan} tag-transform translate`;
-            this.resultados.appendChild(resultadoLinhaB3);
-
-            const resultadoLinhaB4 = document.createElement('li');
-            resultadoLinhaB4.textContent = `service-port vlan ${vlan} gpon ${placa}/${ramo} ont ${nOnu} gemport 126 multi-service user-vlan 110 tag-transform translate`;
-            this.resultados.appendChild(resultadoLinhaB4);
-
-        }
-
-        if (base == 'WLAN' && olt == 'HUAWEI' && olt2 == 'DISTRITO' && lineProfile == 'BRIDGE') {
-
-            const resultadoLinhaB2 = document.createElement('li');
-            resultadoLinhaB2.textContent = `conf`;
-            this.resultados.appendChild(resultadoLinhaB2);
-
-            const resultadoPlaca = document.createElement('li');
-            resultadoPlaca.textContent = `interface gpon ${placa}`;
-            this.resultados.appendChild(resultadoPlaca);
-
-            const resultadoRamo = document.createElement('li');
-            resultadoRamo.textContent = `ont add ${ramo} sn-auth ${serial} omci ont-lineprofile-id 2440 ont-srvprofile-id 2440 desc "${nome}"`;
-            this.resultados.appendChild(resultadoRamo);
-
-            const resultadonOnu = document.createElement('li');
-            resultadonOnu.textContent = `ont port native-vlan ${ramo} ${nOnu} eth 1 vlan ${vlan} priority 0`;
-            this.resultados.appendChild(resultadonOnu);
-
-            const resultadoLinhaB6 = document.createElement('li');
-            resultadoLinhaB6.textContent = `ont port native-vlan ${ramo} ${nOnu} eth 1 vlan 110`;
-            this.resultados.appendChild(resultadoLinhaB6);
-
-            const resultadoLinhaB3 = document.createElement('li');
-            resultadoLinhaB3.textContent = `service-port vlan ${vlan} gpon ${placa}/${ramo} ont ${nOnu} gemport 126 multi-service user-vlan ${vlan} tag-transform translate`;
-            this.resultados.appendChild(resultadoLinhaB3);
-
-            const resultadoLinhaB4 = document.createElement('li');
-            resultadoLinhaB4.textContent = `service-port vlan ${vlan} gpon ${placa}/${ramo} ont ${nOnu} gemport 126 multi-service user-vlan 110 tag-transform translate`;
-            this.resultados.appendChild(resultadoLinhaB4);
-
-        }
-
-        if (base == 'WLAN' && olt == 'HUAWEI' && olt2 == 'ICOARACI' && lineProfile == 'ROUTER') {
-            const resultadoLinhaB2 = document.createElement('li');
-            resultadoLinhaB2.textContent = `conf`;
-            this.resultados.appendChild(resultadoLinhaB2);
-
-            const resultadoPlaca = document.createElement('li');
-            resultadoPlaca.textContent = `interface gpon ${placa}`;
-            this.resultados.appendChild(resultadoPlaca);
-
-            const resultadoRamo = document.createElement('li');
-            resultadoRamo.textContent = `ont add ${ramo} sn-auth ${serial} omci ont-lineprofile-id 2440 ont-srvprofile-id 2440 desc "${nome}"`;
-            this.resultados.appendChild(resultadoRamo);
-
-            const resultadoLinhaB5 = document.createElement('li');
-            resultadoLinhaB5.textContent = `ont ipconfig ${ramo} ${nOnu} dhcp vlan 99 priority 0`;
-            this.resultados.appendChild(resultadoLinhaB5);
-
-            const resultadonOnu = document.createElement('li');
-            resultadonOnu.textContent = `ont port native-vlan ${ramo} ${nOnu} eth 2 vlan ${vlan}`;
-            this.resultados.appendChild(resultadonOnu);
-
-            const resultadoLinhaB6 = document.createElement('li');
-            resultadoLinhaB6.textContent = `ont port native-vlan ${ramo} ${nOnu} eth 2 vlan 99`;
+            resultadoLinhaB6.textContent = `ont tr069-server-config ${ramo} ${nOnu} profile-id 30`;
             this.resultados.appendChild(resultadoLinhaB6);
 
             const resultadoLinhaB7 = document.createElement('li');
@@ -133,13 +66,12 @@ class GetValores {
             this.resultados.appendChild(resultadoLinhaB3);
 
             const resultadoLinhaB4 = document.createElement('li');
-            resultadoLinhaB4.textContent = `service-port vlan ${vlan} gpon ${placa}/${ramo} ont ${nOnu} gemport 126 multi-service user-vlan 99 tag-transform translate`;
+            resultadoLinhaB4.textContent = `service-port vlan 110 gpon ${placa}/${ramo} ont ${nOnu} gemport 126 multi-service user-vlan 110 tag-transform translate`;
             this.resultados.appendChild(resultadoLinhaB4);
-
 
         }
 
-        if (base == 'WLAN' && olt == 'HUAWEI' && olt2 == 'ICOARACI' && lineProfile == 'BRIDGE') {
+        if (base == 'WLAN' && olt == 'HUAWEI' && olt2 == 'WLAN' && lineProfile == 'BRIDGE') {
 
             const resultadoLinhaB2 = document.createElement('li');
             resultadoLinhaB2.textContent = `conf`;
@@ -158,46 +90,22 @@ class GetValores {
             this.resultados.appendChild(resultadonOnu);
 
             const resultadoLinhaB6 = document.createElement('li');
-            resultadoLinhaB6.textContent = `ont port native-vlan ${ramo} ${nOnu} eth 1 vlan 110`;
+            resultadoLinhaB6.textContent = `ont tr069-server-config ${ramo} ${nOnu} profile-id 30`;
             this.resultados.appendChild(resultadoLinhaB6);
 
+            const resultadoLinhaB7 = document.createElement('li');
+            resultadoLinhaB7.textContent = `quit`;
+            this.resultados.appendChild(resultadoLinhaB7);
+
             const resultadoLinhaB3 = document.createElement('li');
-            resultadoLinhaB3.textContent = `service-port vlan ${vlan} gpon ${placa}/${ramo} ont ${nOnu} gemport 126 multi-service user-vlan ${vlan} tag-transform translate`;
+            resultadoLinhaB3.textContent = `service-port vlan 110 gpon ${placa}/${ramo} ont ${nOnu} gemport 126 multi-service user-vlan 110 tag-transform translate`;
             this.resultados.appendChild(resultadoLinhaB3);
 
             const resultadoLinhaB4 = document.createElement('li');
-            resultadoLinhaB4.textContent = `service-port vlan ${vlan} gpon ${placa}/${ramo} ont ${nOnu} gemport 126 multi-service user-vlan 110 tag-transform translate`;
+            resultadoLinhaB4.textContent = `service-port vlan ${vlan} gpon ${placa}/${ramo} ont ${nOnu} gemport 126 multi-service user-vlan ${vlan} tag-transform translate`;
             this.resultados.appendChild(resultadoLinhaB4);
 
         }
-
-        if (base == 'WLAN' && olt == 'HUAWEI' && olt2 == 'CIDADE NOVA' && lineProfile == 'BRIDGE') {
-
-            const resultadoLinhaB2 = document.createElement('li');
-            resultadoLinhaB2.textContent = `conf`;
-            this.resultados.appendChild(resultadoLinhaB2);
-
-            const resultadoPlaca = document.createElement('li');
-            resultadoPlaca.textContent = `interface gpon ${placa}`;
-            this.resultados.appendChild(resultadoPlaca);
-
-            const resultadoRamo = document.createElement('li');
-            resultadoRamo.textContent = `ont add ${ramo} sn-auth ${serial} omci ont-lineprofile-id 30 ont-srvprofile-id 30 desc "${nome}"`;
-            this.resultados.appendChild(resultadoRamo);
-
-            const resultadonOnu = document.createElement('li');
-            resultadonOnu.textContent = `ont port native-vlan ${ramo} ${nOnu} eth 1 vlan ${vlan} priority 0`;
-            this.resultados.appendChild(resultadonOnu);
-
-            const resultadoLinhaB3 = document.createElement('li');
-            resultadoLinhaB3.textContent = `service-port vlan ${vlan} gpon ${placa}/${ramo} ont ${nOnu} gemport 37 multi-service user-vlan ${vlan} tag-transform translate`;
-            this.resultados.appendChild(resultadoLinhaB3);
-
-        }
-
-
-
-
 
     }
 
